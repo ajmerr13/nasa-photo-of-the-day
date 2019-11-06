50bbdbcd6b936ac51816c6ecb221114fa37c3ddc
ajmerr13 <oliverclosoff07@gmail.com>
2019-11-06 17:16:09 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios"
 import "./App.css";
+import Card from "./Components/Card"
 
 function App() {
+  const [nasaImg, setNasaImg] = useState([]);
+  useEffect(() => {
+
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=HeJ9yb3WaxSY1faBvrEJLfH4brmlYkRaYxo3eOPK")
+    .then(res => {
+      console.log(res.data);
+      setNasaImg(res.data);
+    });
+
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+
+<header><h1>NASA potd</h1></header>
+      <Card title={nasaImg.title} url={nasaImg.url} explanation={nasaImg.explanation} date={nasaImg.date} /> 
     </div>
   );
 }
